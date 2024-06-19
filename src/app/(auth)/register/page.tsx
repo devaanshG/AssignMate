59c43453ea2b949a26eee66991daf2422f199805
Devaansh Gupta <devgup@Devaanshs-MacBook-Air.local>
2024-06-20 11:51:45 +1200

    Created front-end for Register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,24 +3,53 @@ export default function Register() {
     <>
       <section className="flex items-center justify-center overflow-hidden">
         <div className="w-[50vw] h-screen flex items-center justify-center">
-          <form action="" className="flex flex-col w-1/2 bg-white-200">
-            <div className="flex mb-5">
-              <div>
+          <form
+            action=""
+            className="flex flex-col w-[70%] shadow-lg rounded-xl p-10 gap-5"
+          >
+            <div className="flex justify-between">
+              <div className="flex flex-col">
                 <label htmlFor="fname">First name</label>
-                <input type="text" name="fname" id="fname" className="w-1/2" />
+                <input
+                  type="text"
+                  name="fname"
+                  id="fname"
+                  className="h-9 rounded-md border-b-2"
+                />
               </div>
-              <div>
+              <div className="flex flex-col">
                 <label htmlFor="lname">Last name</label>
-                <input type="text" name="lname" id="lname" className="w-1/2" />
+                <input
+                  type="text"
+                  name="lname"
+                  id="lname"
+                  className="h-9 rounded-md border-b-2"
+                />
               </div>
             </div>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-            <label htmlFor="dob">Date of birth</label>
-            <input type="date" name="dob" id="dob" />
+            <div className="flex flex-col">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="h-9 rounded-md border-b-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="dob">Date of birth</label>
+              <input
+                type="date"
+                name="dob"
+                id="dob"
+                className="h-9 rounded-md border-b-2"
+              />
+            </div>
           </form>
         </div>
-        <div className="bg-ecstasy-200 w-[50vw] h-screen"></div>
+        <div className="bg-ecstasy-300 w-[50vw] h-screen flex items-center justify-center">
+          <h1 className="text-7xl text-center">Join the Future of Grading</h1>
+        </div>
       </section>
     </>
   );
